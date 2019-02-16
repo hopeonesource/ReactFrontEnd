7159ce0807e0f548e2eaa768e7978a2a34deaa29
github username <github email address>
2019-02-16 16:35:12 -0500

    about routing avail from app component

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import AboutHero from '../components/aboutComponents/AboutHero'
+import OurSponsors from '../components/aboutComponents/OurSponsors'
+import OurTeam from '../components/aboutComponents/OurTeam'
+import WhatWeDo from '../components/aboutComponents/WhatWeDo'
+import WhoWeAre from '../components/aboutComponents/WhoWeAre'
 
 const About = () => {
   return(
     <div>
-      About 
+      About
+      <AboutHero />
+      <OurSponsors />
+      <OurTeam />
+      <WhatWeDo />
+      <WhoWeAre />
     </div>
   )
 }
