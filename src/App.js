0355bdf22f054cb8d946b00cc75a86dea8c519a8
Ryan Aubrey <rma7qb@virginia.edu>
2019-02-16 15:19:54 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ButtonAppBar from './ButtonAppBar';
+import { Typography } from '@material-ui/core';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ButtonAppBar />
+        <Typography variant='h2'>Welcome to Hope One Source</Typography>
       </div>
     );
   }
