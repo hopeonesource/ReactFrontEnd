59bd1af8caa20dffd1f4e53657e1f48759b972a1
Ryan Aubrey <rma7qb@virginia.edu>
2019-02-16 16:17:57 -0500

    Adding react router and example home and about pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './containers/headerContainer.js';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AboutExample from './containers/AboutExample';
+import HomeExample from './containers/HomeExample';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+        <Router>
+          <div>
+            <Header />
+            <Route exact path="/" component={HomeExample} />
+            <Route exact path="/about" component={AboutExample} />
+          </div>
+        </Router>
       </div>
     );
   }
