5eab84563cbcd5d81ce5a6bfd461b823352cea21
Serpentz <abyicherinet@gmail.com>
2019-02-17 12:49:46 -0500

    Hero section Image size and repeat

--- src/components/homeComponents/actionButtonComponent.js
+++ src/components/homeComponents/actionButtonComponent.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
 
 const styles = theme => ({
   fab: {
-    margin: theme.spacing.unit,
+    margin: theme.spacing.unit
   },
   extendedIcon: {
-    marginRight: theme.spacing.unit,
-  },
+    marginRight: theme.spacing.unit
+  }
 });
 
 function ActionButton(props) {
   const { classes } = props;
   return (
     <div>
-      <Fab color="primary" variant="extended" aria-label="Delete" className={classes.fab}>
+      <Fab
+        color="primary"
+        variant="extended"
+        aria-label="Delete"
+        className={classes.fab}
+      >
         {props.text}
       </Fab>
     </div>
