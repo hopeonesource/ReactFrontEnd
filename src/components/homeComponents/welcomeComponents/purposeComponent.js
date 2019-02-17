168a15ed77f4f0d7471f5ad86352efd8097871a2
Drewisatlas <drewisatlas@gmail.com>
2019-02-17 17:48:25 -0500

    built out grid for what we do

--- src/components/homeComponents/welcomeComponents/purposeComponent.js
+++ src/components/homeComponents/welcomeComponents/purposeComponent.js
@@ -1,15 +1,31 @@
 import React from "react";
+import styles from "./Purpose.module.css";
+import phoneImg  from "./iphoneimage.png";
+import gov from "../../../images/govsponsors.png";
 
 function Purpose(props) {
   return (
     <div id="parent">
       <h1> Welcome to HopeOneSource </h1>
-      <h2> What do we do? </h2>
-      <div>
-        {" "}
-        We collaborate with service providers to provide the most up to date &
-        accurate information about available programs and service to our
-        vulnerable neighbors in real-time.{" "}
+      <div className={styles.purposeContainer}>
+        <div className={styles.phoneDiv}><img src={phoneImg} /> </div>
+        <div className={styles.whatWeDo}>
+        <h2>What We Do?</h2>
+					<h5>We collaborate with Service Providers to provide the most up todate & accurate info about available programs & services to our vulnerable neighbors in real-time.</h5>
+         </div>
+        <div className={styles.providers}>
+          <h4>Service Providers</h4>
+          <p>Connect those most vulnerable with your services.</p>
+        </div>
+        <div className={styles.vulnerable}>
+          <h4>Vulnerable Community Members</h4>
+          <p>Connect to housing, career, and social services needed.</p>
+        </div>
+        <div className={styles.local}>
+          <h4>Local Government</h4>
+          <p>Real-time data driven solutions that support the continuum of care.</p>
+        </div>
+
       </div>
     </div>
   );
