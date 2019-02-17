19fd7e84604b1e52bd225f8a46558711e1025598
Drewisatlas <drewisatlas@gmail.com>
2019-02-17 12:20:10 -0500

    updated hero section post progress check

--- src/containers/testHeroContainer.js
+++ src/containers/testHeroContainer.js
@@ -12,30 +12,19 @@ class Hero extends Component {
         {/* <!-- Intro -->*/}
         <section className="view">
 
-          <div className="row">
-
-          <div className="col-md-6">
-
-            <div className="view">
-              <img src="https://uc833620bc9d52d64f2e62f5b9ac.previews.dropboxusercontent.com/p/thumb/AAULuwmKk2DLnnkZA1myrJeGkJpuoYOlgkZzVsLYwGR6AnfwRAeuhTpHLsWsW6uhPKRGvi3VTeWRu1gblq6vMCxqVQL8ObKQ7DwYo2ncvKwTgNrY2iN33m0AdnCVGM1H1zTtkDIoUrCN5SxNaCAqskFb36JYy1euDQJo2T2kcjfXJvYMPRvmARlyrmkMLB01qScysiAWxePTwFj5OIoyn1INWLV1oVbctETBikr0yjy2e9qHTwvRyDUS1xoeNHz-kJfBk3U4cLX5kzHGRx7aktP4/p.png?size_mode=5" className="img-fluid" alt="smaple image"/>
-              <div className="mask flex-center ">
-              </div>
-            </div>
-
-        </div>
-
-            <div className="col-md-6">
+            <div style={{'backgroundImage':'url(https://images.unsplash.com/photo-1543312226-3f1d5eb9f5e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1599&q=80)',
+            "height":"80vh"}}>
 
               <div className="d-flex flex-column justify-content-center h-100">
-                <h1 className="heading display-3">Welcome to HopeOneSource</h1>
-                <h4 className="subheading font-weight-bold">Are You in need of services? Are you a Service Provider? Are you a Web Developer? Do you want to volunteer with us?</h4>
-                <div className="mr-auto">
-                  <button type="button" className="btn btn-lily btn-margin btn-rounded">Get started <i className="fas fa-caret-right ml-3"></i></button>
+                <h1 className="heading display-3" style={{'textAlign':'center', 'color':'white'}}>Welcome to HopeOneSource</h1>
+                <h4 className="subheading font-weight-bold" style={{'textAlign':'center', 'color':'white'}}>Connecting Vulnerable People To Services</h4>
+                <div className="mr-auto" style={{'textAlign':'center', 'color':'white', 'justify-content':'space-between', 'marginLeft':'auto', 'marginRight':'auto'}}>
+                  <button type="button" className="btn btn-lily btn-margin btn-rounded" style={{'margin-left':'5px'}}>Get Services<i className="fas fa-caret-right ml-3"></i></button>
+                  <button type="button" className="btn btn-lily btn-margin btn-rounded" style={{'margin-left':'5px'}}>Provide Services <i className="fas fa-caret-right ml-3"></i></button>
                 </div>
               </div>
 
             </div>
-        </div>
 
         </section>
         {/* <!-- Intro -->*/}
